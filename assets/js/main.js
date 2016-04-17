77ad5d74964f195edf050ec99184199ac8785ee9
Luís Melo <luismelo7@gmail.com>
2016-04-17 20:41:02 +0100

    minor changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,22 +31,9 @@ $('#registration-form').submit(function(e){
 		var postForm = { //Fetch form data
 						'fname'     : $('#registration-form #fname').val(),
 						'email'     : $('#registration-form #email').val(),
-						'type'       : $('#registration-form #type').val()
+						'curso'     : $('#registration-form #curso').val(),
+						'ano'       : $('#registration-form #ano').val()
 		};
-
-		if ( postForm.type == "Estudante" )
-			{
-				  postForm.curso 		= $('#registration-form #curso').val();
-					postForm.ano 			= $('#registration-form #ano').val();
-					postForm.JuniName = "-----";
-			}
-			else if ( postForm.type == "Junior Empresa" )
-			{
-					  postForm.curso 		= "-----";
-						postForm.ano 			= "-----";
-						postForm.JuniName = $('#registration-form #JuniName').val();
-			}
-
 
     $.ajax({
             type      : 'POST',
@@ -85,34 +72,5 @@ $('#registration-form').submit(function(e){
 $( document ).ready(function() {
 	//esconde o botao do video - ainda nao ha
 	$("#video").hide();
-
-  $("#JuniName").hide();
-	$("#JuniName").prop('required', false);
-	$("#ano").hide();
-	$("#ano").prop('required', false);
-	$("#curso").hide();
-	$("#curso").prop('required', false);
-});
-
-
-$( "#type" ).change(function() {
-	if ( $(this).val() == "Estudante" )
-	{
-		  $("#JuniName").hide();
-			$("#JuniName").prop('required', false);
-			$("#ano").show();
-			$("#ano").prop('required', true);
-			$("#curso").show();
-			$("#curso").prop('required', true);
-	}
-	else if ( $(this).val() == "Junior Empresa" )
-	{
-		  $("#JuniName").show();
-			$("#JuniName").prop('required', true);
-			$("#ano").hide();
-			$("#ano").prop('required', false);
-			$("#curso").hide();
-			$("#curso").prop('required', false);
-	}
 
 });
--- assets/php/contact.php
+++ assets/php/contact.php
@@ -4,14 +4,12 @@
 
 	$fname        	= $_POST['fname'];
 	$email        	= $_POST['email'];
-	$type          	= $_POST['type'];
 	$curso		    	= $_POST['curso'];
 	$ano	        	= $_POST['ano'];
-	$JuniName      	= $_POST['JuniName'];
 
-	$message     	= "Name: $fname, \nEmail: $email, \nTipo de Inscricao: $type, \nCurso: $curso, \nAno: $ano, \nNome da Junior Empresa: $JuniName.";
+	$message     	= "Name: $fname, \nEmail: $email, \nCurso: $curso, \nAno: $ano.";
 
-	if(mail('luismelo7@gmail.com', "Test Sub", $message))
+	if(mail('junifeup@junifeup.pt', "Test Sub", $message))
 		$form_data['success'] = true;
 	else
 		$form_data['success'] = false;
