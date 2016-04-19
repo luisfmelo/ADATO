4fc09470c0fef29b159081040b702f326cb6e739
Pedro Cova <pedrocova96@gmail.com>
2016-04-19 23:21:16 +0100

    contact form and mail working

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,18 +1,24 @@
 <?php
+	require 'ligacaoSMTP.php';
+
 	$form_data = [];
 
-	$fname        	= $_POST['fname'];
-	$email        	= $_POST['email'];
-	$curso		    	= $_POST['curso'];
-	$ano	        	= $_POST['ano'];
+	$fname	= $_POST['fname'];
+	$email	= $_POST['email'];
+	$curso	= $_POST['curso'];
+	$ano	= $_POST['ano'];
 
-	$message     	= "Name: $fname, \nEmail: $email, \nCurso: $curso, \nAno: $ano.";
+	$message = "Name: $fname, \nEmail: $email, \nCurso: $curso, \nAno: $ano.";
 
-	if(mail('junifeup@junifeup.pt', "Test Sub", $message))
+	$mail->addAddress('pcova@junifeup.pt');
+	$mail->Subject = 'Test Sub';
+
+	$mail->Body = $message;
+
+	if($mail->send())
 		$form_data['success'] = true;
-	else
+	else {
 		$form_data['success'] = false;
-
+	}
 	echo json_encode($form_data);
 	return;
-?>
