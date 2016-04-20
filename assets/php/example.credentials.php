2aa631baa62cbb4a915ba7b9f3367112d34ab588
Luís Melo <luismelo7@gmail.com>
2016-04-20 18:54:54 +0100

    fix commits

---
+++ assets/php/example.credentials.php
@@ -0,0 +1,7 @@
+<?php
+	$_CREDENTIALS['host'] = 'mail.example.pt';
+	$_CREDENTIALS['auth'] = true;
+	$_CREDENTIALS['username'] = 'example@example.pt';
+	$_CREDENTIALS['password'] = '******';
+	$_CREDENTIALS['SMTPSecure'] = 'tls';
+	$_CREDENTIALS['port'] = 25;
