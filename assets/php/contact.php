2c8a387e80c6b02aae0a653e6e1c2c698cfb5b7d
Luís Melo <luismelo7@gmail.com>
2016-05-12 22:32:42 +0100

    changed charset to utf8

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -42,8 +42,9 @@
   	$message = "Name: $fname \nEmail: $email \nCurso: $curso \nAno: $ano \nHobbie1: $h1 \nHobbie2: $h2 \nHobbie3: $h3 \nLinkedin: $linkedin";
 
 	$mail->addAddress('adato@junifeup.pt');
-  	$mail->addAttachment( $target_file , 'cv.pdf' );
+  $mail->addAttachment( $target_file , 'cv.pdf' );
 	$mail->addAttachment('../images/credentials/'.$email.'.jpeg', 'credential.jpeg');
+	$mail->CharSet = 'UTF-8';
 	$mail->Subject = 'Registo AD@TO';
 
 	$mail->Body = $message;
