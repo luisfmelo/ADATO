197fa79c815327e6ecf70e4c02d6cdf9431f73aa
Pedro Cova <pedrocova96@gmail.com>
2016-05-13 00:07:10 +0100

    debug

--- assets/php/contact.php
+++ assets/php/contact.php
@@ -1,6 +1,9 @@
 <?php
+	echo 1;
 	require 'ligacaoSMTP.php';
+	echo 2;
 	require 'generate_img.php';
+	echo 3;
 	$form_data = ['success' => true];
 	$fname	= $_POST['fname'];
 	$email	= $_POST['email'];
@@ -10,7 +13,9 @@
 	$h1	= $_POST['hobbie1'];
 	$h2	= $_POST['hobbie2'];
 	$h3	= $_POST['hobbie3'];
+	echo 4;
 	generate_credential($fname, $email, $curso, $h1, $h2, $h3);
+	echo 5;
 	if (isset($_FILES['cv'])) {
 		$fileType = pathinfo(basename($_FILES["cv"]["name"]), PATHINFO_EXTENSION);
 		$target_dir = '../cv/';
@@ -30,13 +35,15 @@
 			return;
 		}
 	}
+	echo 6;
   	$message = "Name: $fname \nEmail: $email \nCurso: $curso \nAno: $ano \nHobbie1: $h1 \nHobbie2: $h2 \nHobbie3: $h3 \nLinkedin: $linkedin";
-	$mail->addAddress('adato@junifeup.pt');
+	$mail->addAddress('pcova@junifeup.pt');
   $mail->addAttachment( $target_file , 'cv.pdf' );
 	$mail->addAttachment('../images/credentials/'.$email.'.jpeg', 'credential.jpeg');
 	$mail->CharSet = 'UTF-8';
 	$mail->Subject = 'Registo AD@TO';
 	$mail->Body = $message;
+	echo 7;
 	if($mail->send()) {
 		$form_data['success'] = true;
 		$replymsg = "Obrigado $fname pelo teu registo!\n\nEstamos a processar a tua inscrição.\nDaqui a nada entraremos em contacto contigo!\n\n\nAD@TO - sem bancas, sem gravata, sem complicações - a tua carreira,  o teu futuro\n\n\nJuniFEUP";
@@ -47,5 +54,6 @@
 		$mail->Body = $replymsg;
 		$mail->send();
 	}
+	echo 8;
 	header("Location: http://$_SERVER[HTTP_HOST]?success=".$form_data['success']."#registration");
 	die();
